bebbaa99aa606a57bbe95e73b8e68b719425175b
guoyanbin <wwwgybcom@sina.com>
2023-11-22 10:12:52 +0800

    图书图片

--- src/main/java/com/example/shop/util/ImagePathUtil.java
+++ src/main/java/com/example/shop/util/ImagePathUtil.java
@@ -2,13 +2,15 @@ package com.example.shop.util;
 
 import org.springframework.boot.system.ApplicationHome;
 
+import io.micrometer.common.util.StringUtils;
+
 public class ImagePathUtil {
 	private static final String sep = System.getProperty("file.separator");
-	private static final String IMAGE_URI = sep + "src" + sep + "main" + sep + "resources" + sep + "image" + sep;
+	private static final String IMAGE_URI = sep + "image" + sep;
 	private static String IMAGE_URL = "";
 	
     public static String getImagePath() {
-		if("".equals(IMAGE_URL)) {
+		if(StringUtils.isBlank(IMAGE_URL)) {
 			ImagePathUtil u = new ImagePathUtil();
 			u.init();
 		}
@@ -17,7 +19,7 @@ public class ImagePathUtil {
     
     private void init() {
     	ApplicationHome applicationHome = new ApplicationHome(getClass());
-        IMAGE_URL = applicationHome.getSource().getParentFile().getParent().toString() + IMAGE_URI;
+        IMAGE_URL = applicationHome.getSource().getParentFile().toString() + IMAGE_URI;
     }
 
 }
